8b56eb68567cef2317a2fd6033dd005b64893d7d
KartikPandey19 <kartik2000pandey@gmail.com>
2025-08-16 12:14:12 +0530

    ui changes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Home } from './components/Home'
+import './App.css'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./common/Header"
+import { HomeScreen } from "./Landing/HomeScreen"
+export function Home() {
+  
+    return(
+        <>
+        <Header />
+        <HomeScreen />
+        </>
+    )
+}
